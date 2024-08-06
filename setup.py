bf39e33af600e24385906088ac0c0721e8351671
Somanath <kingsomanath444@gmail.com>
2024-08-06 14:44:28 +0530

    requirments and setup updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='signLanguage',
+    version='0.0.0',
+    author='somanth444',
+    author_email='kingsomanath444@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
